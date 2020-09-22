2e46a11da856a0c54dfbdbd7714907c5618e20c2
royFloresNyc <royfloresnyc@gmail.com>
2020-09-22 10:25:34 -0400

    Render registerd dogs on a table

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 document.addEventListener('DOMContentLoaded', () => {
-
+    getDogs()
 })
+
+const BASE_URL = "http://localhost:3000/dogs/"
+
+const getDogs = () => {
+    fetch(BASE_URL)
+    .then(resp => resp.json())
+    .then(data => {
+        renderDogs(data)
+    })
+}
+
+const renderDogs = (dogCollection) => {
+    for (const dog of dogCollection) {
+        renderDog(dog)
+    }
+}
+
+const renderDog = (dog) => {
+    const dogTable = document.querySelector('#table-body')
+    const dogRow = createDogRow(dog)
+    dogTable.append(dogRow)
+}
+
+const createDogRow = (dog) => {
+    const dogRow = document.createElement('tr')
+    dogRow.innerHTML = `
+        <td>${dog.name}</td> 
+        <td>${dog.breed}</td> 
+        <td>${dog.sex}</td> 
+        <td><button data-dog-id= "${dog.id}">Edit Dog</button></td>
+    `
+    return dogRow 
+}
