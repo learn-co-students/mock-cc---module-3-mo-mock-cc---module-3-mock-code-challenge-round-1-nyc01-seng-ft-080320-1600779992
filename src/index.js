4c11978069786c31e781a56857cfbb7fdc2cfcc9
CanniJr <cannigialaluw@gmail.com>
2020-09-22 11:41:44 -0400

    Final commit

--- src/index.js
+++ src/index.js
@@ -7,18 +7,64 @@ const DOGS_URL = "http://localhost:3000/dogs"
 const getDogs = () => {
     fetch(DOGS_URL)
     .then(response => response.json())
-    .then(data => console.log(data))
+    .then(data => renderDogs(data))
 }
-getDogs()
+
 
 const renderDogs = (dogCollection) => { // this renders a list of all dogs
     for (dog of dogCollection){
         //for each dog of the dogCollection data
-        const dogLi = document.createElement('li')
-        dogLi.textContent = dog.name
-        dogLi.dataset.dogId = dog.id
+        const dogTr = document.createElement('tr')
+        dogTr.textContent = dog.name
+        dogTr.dataset.dogId = dog.id
 
-        const dogUl = document.createElement('ul')
-        
+        const dogList = document.querySelector('#table-body')
+        // console.log(dogId)
+        dogList.append(dogTr)
     }
+}
+
+// Create renderDog for individual dogs details
+const renderDog = dog => { // shows the dog info
+    const dog 
+}
+
+const clickHandler = () => {
+    document.addEventListener('click', e => {
+        
+    })
+}
+
+const submitHandler = () => {  // handling event listener for submit
+    const form = document.querySelector('#dog-form')
+
+    form.addEventListener('submit', e =>{
+        e.preventDefault()
+
+        const name = form.name.value
+        const breed = form.breed.value
+        const sex = form.sex.value
+
+        const dog = {
+            name: name,
+            breed: breed,
+            sex: sex,
+        }
+
+        const options ={
+            method: "PATCH",
+            headers: {
+                "content-type" : "application/json",
+                "accept" : "application/json"
+            },
+            body: JSON.stringify(dog)
+        }
+        fetch(DOGS_URL, options)
+        .then(response => response.json())
+        .then(dog => console.log(dog))
+    })
+
+    getDogs()
+    submitHandler()
+    clickHandler()
 }
