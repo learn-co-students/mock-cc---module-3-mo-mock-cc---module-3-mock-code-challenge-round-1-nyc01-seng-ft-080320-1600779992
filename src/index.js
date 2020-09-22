ccb86af2ca7ba4653ec8888727db5806e4fb0b14
https://learn.co/arf2k <foreman.alexander@gmail.com>
2020-09-22 10:14:55 -0400

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+function fetchDogs(){
+     fetch("http://localhost:3000/dogs")
+     .then(resp => resp.json())
+     .then(dogs => {
+          renderDogs(dogs)   
+     })
+}
+
+function renderDogs(dogs){
+
+
+}
+
+
+
+
+
+
+fetchDogs()
 })
