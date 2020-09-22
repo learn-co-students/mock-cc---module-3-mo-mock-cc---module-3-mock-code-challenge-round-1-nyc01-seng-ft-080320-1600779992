1f376cdd0d260620a6888da9e343538858d7ca12
AmSmo <adam.smolenski@gmail.com>
2020-09-22 11:14:49 -0400

    deliverables

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ function renderDog(dog){
     const table = document.getElementById("table-body")
     
     let newRow = document.createElement("tr")
-    let dogRow = `<td data-dog-name=${dog.name} >${dog.name}</td> <td class="breed">${dog.breed}</td> <td>${dog.sex}</td> <td><button class="edit" data-dog-id=${dog.id} data-dog-name=${dog.name} data-dog-breed=${dog.breed} data-dog-sex=${dog.sex} name="submit">Edit Dog</button></td>`
+    let dogRow = `<td class="name" data-dog-name="${dog.name}" >${dog.name}</td> <td class="breed">${dog.breed}</td> <td>${dog.sex}</td> <td><button class="edit" data-dog-id=${dog.id} data-dog-name=${dog.name} data-dog-breed="${dog.breed}" data-dog-sex=${dog.sex} name="submit">Edit Dog</button></td>`
     newRow.innerHTML = dogRow
     table.append(newRow)
     // <tr><td>Dog *Name*</td> <td>*Dog Breed*</td> <td>*Dog Sex*</td> <td><button>Edit</button></td></tr>
@@ -55,11 +55,11 @@ function submitHandler(){
 }
 
 function fillForm(button){
-    let breed = (button.parentElement.parentElement.querySelector(".breed")).textContent
+    
     const form = document.getElementById("dog-form")
     const submit = form.querySelector(`[type="submit"]`)
     form.name.value = button.dataset.dogName
-    form.breed.value = breed
+    form.breed.value = button.dataset.dogBreed
     form.sex.value = button.dataset.dogSex
     submit.classList = button.dataset.dogId
 }
