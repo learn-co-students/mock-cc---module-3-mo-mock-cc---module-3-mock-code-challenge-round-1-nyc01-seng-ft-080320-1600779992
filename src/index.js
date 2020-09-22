70ae2fb5f3468367fa8bd12617e5da3563208e12
royFloresNyc <royfloresnyc@gmail.com>
2020-09-22 11:02:06 -0400

    Render dog info on form when edit button is clicked

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     getDogs()
+    clickHandler()
 })
 
 const BASE_URL = "http://localhost:3000/dogs/"
@@ -27,10 +28,41 @@ const renderDog = (dog) => {
 const createDogRow = (dog) => {
     const dogRow = document.createElement('tr')
     dogRow.innerHTML = `
-        <td>${dog.name}</td> 
-        <td>${dog.breed}</td> 
-        <td>${dog.sex}</td> 
+        <td name="name">${dog.name}</td> 
+        <td name="breed">${dog.breed}</td> 
+        <td name="sex">${dog.sex}</td> 
         <td><button data-dog-id= "${dog.id}">Edit Dog</button></td>
     `
     return dogRow 
 }
+
+const clickHandler = () => {
+    document.addEventListener('click', e => {
+        if (e.target.matches('button')) {
+            const dogRow = e.target.closest('tr')
+            const dogId = e.target.dataset.dogId
+            // console.log(dogRow)
+            populateForm(dogRow, dogId)
+        }
+    })
+}
+
+const populateForm = (dogRow, dogId) => {
+    const dog = getDogInfo(dogRow, dogId)
+    const form = document.querySelector('#dog-form')
+    form.name.value = dog.name
+    form.breed.value = dog.breed
+    form.sex.value = dog.sex
+    const submitButton = form.lastElementChild
+    submitButton.dataset.dogId = dog.id
+}
+
+const getDogInfo = (dogRow, dogId) => {
+    const dog = {}
+    dog.id = dogId
+    dog.name = dogRow.querySelector('td[name="name"]').textContent
+    dog.breed = dogRow.querySelector('td[name="breed"]').textContent
+    dog.sex = dogRow.querySelector('td[name="sex"]').textContent
+    return dog
+}
+
