3a46278f8dbbaf4bf1334bd4004b8e62ec9c7d27
dkim61 <dkim187@binghamton.edu>
2020-09-22 14:01:22 -0400

    first attempt

--- src/index.js
+++ src/index.js
@@ -1,98 +1,65 @@
-document.addEventListener('DOMContentLoaded', e => {
+document.addEventListener('DOMContentLoaded', () => {
 
-    const qs = (selector) => document.querySelector(selector)
-    const ce = (element) => document.createElement(element)
-
-    const URL = 'http://localhost:3000/dogs/'
-
-    fetch(URL)
+    const dogsUrl = 'http://localhost:3000/dogs'
+    
+    const table = document.querySelector('#table-body')
+    
+        fetch(dogsUrl)
         .then(resp => resp.json())
-        .then(json => renderDogs(json))
-
-    const renderDogs = (dogs) => {
-        for (dog of dogs) {
-            const newDog = ce('tr')
+        .then(dogs => dogs.forEach(dog => renderDogs(dog)))
     
-            const newName = ce('td')
-            newName.textContent = dog.name
-            newDog.append(newName)
-    
-            const newBreed = ce('td')
-            newBreed.textContent = dog.breed
-            newDog.append(newBreed)
-
-            const newSex = ce('td')
-            newSex.textContent = dog.sex
-            newDog.append(newSex)
-    
-            const newButtonTd = ce('td')
-            newDog.append(newButtonTd)
-    
-            const newButton = ce('button')
-            newButton.textContent = "Edit"
-            newButton.dataset.id = dog.id
-            newButton.className = 'edit-btn'
-            newButtonTd.append(newButton)
-    
-            document.getElementById('table-body').append(newDog)
+        const renderDogs = (dog) => {
+            const dogTR = document.createElement('tr')
+            dogTR.dataset.num = dog.id
+            id = dog.id
+            dogTR.innerHTML = `
+            <td>${dog.name}</td> <td>${dog.breed}</td> <td>${dog.sex}</td> <td><button>Edit</button></td>
+            `
+            table.appendChild(dogTR)
         }
-    }
-
-    const clickHandler = () => {
-        document.addEventListener('click', e => {
-            if (e.target.matches('.edit-btn')) {
-                console.log(e.target)
-                let button = e.target
-                let dogId = button.dataset.id
-                fetchDog(dogId)
-            }
-        })
-    }
-
-    const fetchDog = (dogId) => {
-        fetch(URL + dogId) 
-            .then(resp => resp.json())
-            .then(editDog)
-        }
-
-    const editDog = (dog) =>{
-        const form = qs('#dog-form')
-        form.name.value = `${dog.name}`
-        form.breed.value = `${dog.breed}`
-        form.sex.value = `${dog.sex}`
-        submitHandler(dogId)
-    }
     
-    const submitHandler = () => {
-        document.addEventListener('submit', e => {
-            e.preventDefault()
-            if (e.target.matches('#dog-form')) {
-                const dogForm = e.target
-                patchDogs(dogForm)
-            } 
-        })}
-
-    const patchDogs = (form, reset = false) => {
-        let id = form.id.value // cant seem to find a way to get the id from the button dataset into this
-        
-        const newDog = {
-            "name": form.name.value,
-            "breed": form.breed.value,
-            "sex": form.sex.value
-        }
-        
-        const options = {
-            method: 'PATCH',
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(newDog)
-        }
-        
-        fetch(URL + id, options)
-            .then(resp => resp.json())
-            .then(renderDogs)
-    }
-
-clickHandler()
-})//dom content loaded
+        // const clickHandler = () => {
+        //     document.addEventListener("click", e => {
+        //         if (e.target.tagName === "button")
+        //         id = parseInt(e.target.parentNode.dataset.num)
+        //         editDog(id)
+        //     })
+        // }
+    
+        // const editDog = (id) => {
+        //     fetch(dogsUrl + `/${id}`)
+        //         .then(resp => resp.json())
+        //         .then(dog => postDogOnForm(dog))
+            
+        //     const postDogOnForm = dog => {
+        //     const dogForm = document.querySelector("#dog-form")
+        //         dogForm.dataset.num = dog.id
+        //         dogForm.name.value = dog.name
+        //         dogForm.breed.value = dog.breed
+        //         dogForm.sex.value = dog.sex
+        //     }
+        // }
+    
+        // const submitHandling = () => {
+        //     document.addEventListener("submit", e => {
+        //         e.preventDefault()
+    
+        //         const dogForm = document.querySelector("#dog-form")
+                
+        //         let dog = {
+        //             "name":dogForm.name.value,
+        //             "breed":dogForm.breed.value,
+        //             "sex":dogForm.sex.value
+        //         }
+                
+        //         dogForm.dataset.num = dog.id
+        //         updateDog(dog.id)
+        //         dogForm.reset()
+        //     })
+        // }
+    
+        // const updateDog = (id) => {
+           
+        // }
+    
+    })
