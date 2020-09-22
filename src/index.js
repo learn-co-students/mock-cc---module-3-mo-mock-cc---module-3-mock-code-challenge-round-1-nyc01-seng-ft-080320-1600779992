04e77dc12d72f774185ead874717e1353dabcab4
alerner1 <alyssplerner@gmail.com>
2020-09-22 10:40:36 -0400

    can edit dogs

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
+const getDogs = () => {
+  fetch('http://localhost:3000/dogs')
+    .then(resp => resp.json())
+    .then(json => renderDogs(json));
+}
+
 const renderDogs = (dogs) => {
   const tableBody = document.querySelector('#table-body');
+
+  while (tableBody.firstChild) {
+    tableBody.firstChild.remove();
+  }
+
   for (dog of dogs) {
     const newRow = document.createElement('tr');
+    newRow.dataset.dogId = dog.id;
     
     const newName = document.createElement('td');
     newName.textContent = dog.name;
@@ -26,23 +38,51 @@ const renderDogs = (dogs) => {
 const clickHandler = () => {
   document.addEventListener('click', e => {
     if (e.target.matches('button')) {
-      console.log('button clicked')
+      const nameTd = e.target.parentNode.firstChild;
+      const dogName = nameTd.textContent;
+      const breedTd = nameTd.nextSibling;
+      const dogBreed = breedTd.textContent;
+      const sexTd = breedTd.nextSibling;
+      const dogSex = sexTd.textContent;
+      
+      const dogForm = document.querySelector('#dog-form');
+      dogForm.dataset.dogId = e.target.parentNode.dataset.dogId;
+      const inputFields = document.querySelectorAll('input');
+      inputFields[0].value = dogName;
+      inputFields[1].value = dogBreed;
+      inputFields[2].value = dogSex;
     }
   });
 
   document.addEventListener('submit', e => {
     e.preventDefault();
-    console.log('submitted')
+    const dogForm = document.querySelector('#dog-form');
+    const dogId = dogForm.dataset.dogId;
+    const inputFields = document.querySelectorAll('input');
+    const dogName = inputFields[0].value;
+    const dogBreed = inputFields[1].value;
+    const dogSex = inputFields[2].value;
+    
+    fetch(`http://localhost:3000/dogs/${dogId}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify({
+        name: dogName,
+        breed: dogBreed,
+        sex: dogSex
+      })
+    })
+      .then(resp => resp.json())
+      .then(json => getDogs());
   });
 };
 
 document.addEventListener('DOMContentLoaded', () => {
 
-  // keeping this fetch request inline for now -- might try to figure out how 
-  // to make it its own function later, but it's complicated to get working
-  fetch('http://localhost:3000/dogs')
-    .then(resp => resp.json())
-    .then(json => renderDogs(json));
+  getDogs();
 
   clickHandler();
 });
