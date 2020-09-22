369861f0b5fd3258cbc143d01e25b890ccbbe9d6
CanniJr <cannigialaluw@gmail.com>
2020-09-22 10:37:37 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener('DOMContentLoaded', () => {
 
 })
+
+const DOGS_URL = "http://localhost:3000/dogs"
+
+const getDogs = () => {
+    fetch(DOGS_URL)
+    .then(response => response.json())
+    .then(data => console.log(data))
+}
+getDogs()
+
+const renderDogs = (dogCollection) => { // this renders a list of all dogs
+    for (dog of dogCollection){
+        //for each dog of the dogCollection data
+        const dogLi = document.createElement('li')
+        dogLi.textContent = dog.name
+        dogLi.dataset.dogId = dog.id
+
+        const dogUl = document.createElement('ul')
+        
+    }
+}
