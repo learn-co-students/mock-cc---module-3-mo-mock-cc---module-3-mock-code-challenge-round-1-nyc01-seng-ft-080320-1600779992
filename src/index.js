e7992bf35ed4fd354d95e00f708685c0c93e33db
anickel101 <anson.nickel@gmail.com>
2020-09-22 10:17:20 -0400

    2/5 Deliverables

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
+const DOGS_BASE_URL = "http://localhost:3000/dogs"
+
 document.addEventListener('DOMContentLoaded', () => {
 
+    const getDogs = () => {
+        fetch(DOGS_BASE_URL)
+        .then(resp => resp.json())
+        .then(dogs => renderDogs(dogs))
+    }
+
+    const renderDogs = dogs => {
+        dogs.map(renderDog)
+    }
+
+    const renderDog = dog => {
+        const table = document.querySelector("#table-body")
+        const tableRow = document.createElement("tr")
+        tableRow.innerHTML = `
+        <td>${dog.name}</td>
+        <td>${dog.breed}</td>
+        <td>${dog.sex}</td>
+        <td><button>Edit</button></td>
+        `
+        table.append(tableRow)
+    }
+
+
+    getDogs();
 })
+
+{/* <tbody id="table-body"> */}
