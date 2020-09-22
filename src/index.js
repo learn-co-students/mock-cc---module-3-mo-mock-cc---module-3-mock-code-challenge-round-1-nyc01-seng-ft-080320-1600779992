0b1b77f9258493b71bc2bebc026ccc66ff148b01
kevinpark07 <kevin.park07@yahoo.com>
2020-09-22 14:35:32 -0400

    deliverables

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
 
     const baseUrl = "http://localhost:3000/dogs/"
-    //render a list of registerd dogs
-    //fetch request to url and get dogs
-    //function to take json info and input into table
+    
+    const removeDogs = () => {
+        const dogTable = document.querySelector("#table-body");
+        dogTable.innerHTML = ""
+    }
+    
+    const fetchDogs = () => {
+        removeDogs();
 
-    const fetchDogs = url => {
-        
-        fetch(url)
+        fetch(baseUrl)
         .then(response => response.json())
         .then(dogData => {
             for(dog of dogData) {
@@ -32,94 +35,74 @@ document.addEventListener('DOMContentLoaded', () => {
     const clickHandler = () => {
         document.addEventListener("click", e => {
             if (e.target.matches(`[data-id="edit-button"]`)) {
-                const dogObj = buildDogObjectForForm(e.target);
-                putDogObjectInForm(dogObj);
-                patchUpdate(baseUrl, e.target)}
-
-                //add a function that will send a patch request
-                //function to populate values into the form
-             
-            // else if (e.target.matches(`[type="submit"]`))
-            //     e.preventDefault()
-            //     console.log("I work!")
+                buildDogObjectForForm(e.target);
+                
+            }
         })
     }
 
     
-            //create a patch request for the input items
-            //function to find the dog to update the correct information
                 
-    
-
-    const formValues = () => {
-        const form = document.querySelector("#dog-form");
-        
-        const dogObj = {
-            name: form.name.value,
-            breed: form.breed.value,
-            sex: form.sex.value
-        }
-        return dogObj
-    }
-
-    const patchUpdate = (url,dog) => {
-    
-        const submitHandler = () => {
+                
+    const submitHandler = () => {
         document.addEventListener("submit", e => {
-    
+            e.preventDefault();
+            patchUpdate(e.target);
+        })
+    }
+  
 
-        const dogTd = dog.parentElement;
-        const dogTr = dogTd.parentElement;
-        const dogId = dogTr.dataset.dogId;
+    const patchUpdate = (dogForm) => {
+        const form = dogForm;
+        const dogId = form.dataset.id;
+        const name = form.name.value;
+        const breed = form.breed.value;
+        const sex = form.sex.value;
 
-        const values = formValues()
+        const dog = {
+            name: name,
+            breed: breed,
+            sex: sex
+        };
 
-        
         const options = {
             method: "PATCH",
             headers: {
                 "content-type": "application/json",
                 "accept": "application/json"
             },
-            body: JSON.stringify(values)
+            body: JSON.stringify(dog)
         };
-
-        fetch(url + dogId, options)
+  
+        fetch(baseUrl+dogId, options)
         .then(response => response.json())
-        .then(dogObject => console.log(dogObject))
+        .then(dogObject => {
+            fetchDogs();
+            form.reset();
         })
+    
     }
-    submitHandler()
-}
+
 
     const buildDogObjectForForm = dog => {
         const buttonTd = dog.parentElement;
         const dogTr = buttonTd.parentElement;
+        const dogId = dogTr.dataset.dogId
         const dogTds = dogTr.querySelectorAll('td');
         const dogName = dogTds[0].textContent;
         const dogBreed = dogTds[1].textContent;
         const dogSex = dogTds[2].textContent;
         
-        const dogObj = {
-            name: dogName,
-            breed: dogBreed,
-            sex: dogSex
-        }
-        
-        return dogObj;
-
-    }
-
-    const putDogObjectInForm = dogObj => {
         const form = document.querySelector("#dog-form")
-        form.name.value = dogObj.name;
-        form.breed.value = dogObj.breed;
-        form.sex.value = dogObj.sex;
-        console.log(form)
+        form.name.value = dogName;
+        form.breed.value = dogBreed;
+        form.sex.value = dogSex;
+
+        form.dataset.id = dogId;
     }
 
 
-    fetchDogs(baseUrl);
+    fetchDogs();
     clickHandler();
-    //submitHandler();
+    submitHandler();
 })
